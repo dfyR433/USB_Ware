475569ddf81b5d14fa2507a61851946f5d1b3072
dfyR433 <213033816+dfyR433@users.noreply.github.com>
2025-10-20 18:43:30 +0300

    Update latest_release.ino

--- latest_release/latest_release.ino
+++ latest_release/latest_release.ino
@@ -10,33 +10,10 @@ DNSServer dnsServer;
 WebServer server(80);
 
 void handleSet() {
-  String feature = server.hasArg("feature") ? server.arg("feature") : "";
   String customScript = server.hasArg("customScript") ? server.arg("customScript") : "";
-
-  if (feature == "Richroll") {
-    pingSite();
-  } else if (feature == "Python Script") {
-      py_script();
-  } else if (feature == "Script + Ping") {
-      py_script();
-      pingSite();
-  } else if (feature == "Barrel Roll") {
-      barrelRollMode();
-  } else if (feature == "Notepad") {
-      notepadMode();
-  } else if (feature == "All") {
-      py_script();
-      delay(1000);
-      barrelRollMode();
-      delay(3000);
-      pingSite();
-      delay(1000);
-      notepadMode();
-  } else if (feature == "Custom Script") {
-      runDuckyScript(customScript);
-  }
-
-  server.send(200, "text/plain", feature + " Executed successfully.");
+  
+  DuckyScript(customScript);
+  server.send(200, "text/plain", "Executed successfully.");
 }
 
 void startAP() {
@@ -85,7 +62,7 @@ void startAP() {
 }
 
 void setup() {
-  startUSB("Keyboard");
+  startUSB("Both");
   startAP();
 }
 
